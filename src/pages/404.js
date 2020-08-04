5a9bbfbab76489c81652dc7712ebfb6727479407
Shrijal Tamrakar <tamrakarshrijal@gmail.com>
2020-08-04 12:19:30 +0545

    Created Simple Protfolio

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,15 @@ import React from "react"
 
 export default function NotFound() {
   return (
-    <>
-      <div>
-        <h1>Page not Found!!!</h1>
-      </div>
-    </>
+    <div style={{ color: "white", textAlign: "center" }}>
+      <h1>Page not Found...</h1>
+      <p style={{ paddingTop: "15px" }}>
+        Click{" "}
+        <a href="/" style={{ border: "1px red solid", padding: "5px" }}>
+          here
+        </a>{" "}
+        to be found!
+      </p>
+    </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,71 @@ import React from "react"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world!</p>
-      <img src="http://source.unsplash.com/random/400x200" alt="" />
+    <div className="main-container" style={{ color: `White` }}>
+      <div className="header-container">
+        <h1>Hi I'm Shrijal Tamrakar</h1>
+      </div>
+      <p className="description">
+        <strong>Software Engineer</strong> | Gamer | Football Enthusiast
+      </p>
+      <div className="links">
+        <ul>
+          <li>
+            <a
+              href="https://github.com/shrijaltamrakar"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GITHUB
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:shrijaltamrakar@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              EMAIL
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/shrijaltamrakar/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LINKEDIN
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/shrijal.tamrakar/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              INSTAGRAM
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://dev.to/shrijaltamrakar"
+              target="_blank"
+              rel="noreferrer"
+            >
+              BLOG
+            </a>
+          </li>
+          {/* <li>
+            <a href="#">RESUME</a>
+          </li> */}
+        </ul>
+        {/* <p>Github</p>
+        <p>Email</p>
+        <p>Blog</p>
+        <p>Instagram</p>
+        <p>Facebook</p>
+        <p>Resume</p> */}
+      </div>
     </div>
   )
 }
