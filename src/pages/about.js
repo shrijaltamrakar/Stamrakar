23c41b3c229ea7db9ae78103eb8d63e492569e19
Shrijal Tamrakar <tamrakarshrijal@gmail.com>
2020-08-04 11:05:00 +0545

    Gatsby Pratice

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "./header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" /> <p>Such wow. Very React.</p>
+    </div>
+  )
+}
